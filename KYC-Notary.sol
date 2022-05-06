98360346a991c37ed21daeb9c8512ed50b476b39
Q <rqjones@asu.edu>
2022-05-06 13:50:55 -0700

    added new functions &  modifier Authorized_Users

--- KYC-Notary.sol
+++ KYC-Notary.sol
@@ -37,14 +37,19 @@ interface KYC_Interface{
 contract KYC is KYC_Interface,ERC1155{
     uint256 private constant Key = 0;
     address public KYC_NotaryAddress;
-
-    string private legal_Name;
+    //Any KYC user data can be placed here
+    string public legal_Name;
     string private permanentAddress;
     string private passport;
     string private SSN; 
     string private driversLicenceNumber; 
-    //sets Notary address and ammount of keys
-    constructor(address _KYC_NotaryAddress, uint keyAmmount) ERC1155("KYC contract"){
+    //checks if authorized users hold their token to view data
+    modifier Authorized_Users{
+        require(balanceOf(msg.sender, Key) > 0 || Notary(KYC_NotaryAddress).balanceOf(msg.sender, Key) > 0,"you are not the holder of this KYC contract");
+        _;
+    }
+    //launches contracts and variables
+    constructor(address _KYC_NotaryAddress, uint keyAmmount, string memory legal_Name,string memory permanentAddress,string memory passport,string memory SSN,string memory driversLicenceNumber) ERC1155("KYC contract"){
         KYC_NotaryAddress =_KYC_NotaryAddress;
         _mint(msg.sender, Key,keyAmmount, "");
     }
@@ -52,5 +57,9 @@ contract KYC is KYC_Interface,ERC1155{
     function check4Token() public view returns(bool){
         require(balanceOf(msg.sender, Key) > 0,"you are not the holder of this KYC contract");
         return true;
+    }
+    //check data contents
+    function ViewData() public view Authorized_Users returns(string memory ,string memory,string memory,string memory,string memory){
+        return (legal_Name,permanentAddress,passport,SSN,driversLicenceNumber);
     }
 }
